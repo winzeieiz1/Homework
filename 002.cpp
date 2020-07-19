11aa2f8dd5164b30a06bc7c9178ca6076f19cd6d
winzeieiz1 <68474657+winzeieiz1@users.noreply.github.com>
2020-07-19 12:49:57 +0700

    Add files via upload

---
+++ 002.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << endl;
+	cout << " 1st Term of 2020\t\\45132144  Wanthanee Prachuabsupakij\n";
+	cout << endl;
+	cout << endl;
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "| Subject ID | Subject name\t\t| score (100)| Grad  |\n";
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "|  4570034   | Introduction to computer |     85     |  \"A\"  |\n";
+	cout << "|  4580247   | English                  |     74     |  \"B\"  |\n";
+	cout << "|  4570034   | Design Thinking          |     92     |  \"A\"  |\n";
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "|  TOTAL     | Subject = 3              |    Average = 3.87  |\n";
+	cout << "======================================" << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "KMUTNB"<<endl;
+	return 0;
+}
